ac9af91b47130c230eb6ac60ff532cab8e6fe6a6
Toshimichi0915 <main@toshimichi.net>
2020-11-30 06:18:31 +0900

    Add sound effect for shield break

--- src/main/java/net/toshimichi/dungeons/listeners/ShieldListener.java
+++ src/main/java/net/toshimichi/dungeons/listeners/ShieldListener.java
@@ -1,21 +1,38 @@
 package net.toshimichi.dungeons.listeners;
 
+import net.toshimichi.dungeons.DungeonsPlugin;
 import net.toshimichi.dungeons.events.PlayerShieldEvent;
+import net.toshimichi.dungeons.nat.api.CooldownUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class ShieldListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onDamage(EntityDamageByEntityEvent e) {
         if (!(e.getEntity() instanceof HumanEntity)) return;
         if (!((HumanEntity) e.getEntity()).isBlocking()) return;
         if (e.getDamage() == 0 || e.getFinalDamage() != 0) return;
         PlayerShieldEvent fake = new PlayerShieldEvent(e);
         Bukkit.getPluginManager().callEvent(fake);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onShieldHit(PlayerShieldEvent e) {
+        if (!(e.getEntity() instanceof Player)) return;
+        Bukkit.getScheduler().runTaskLater(DungeonsPlugin.getPlugin(), () -> {
+            CooldownUtils utils = Bukkit.getServicesManager().load(CooldownUtils.class);
+            int cooldown = utils.getCooldown((Player) e.getEntity(), Material.SHIELD);
+            if (cooldown > 0)
+                e.getEntity().getWorld().playSound(e.getEntity().getLocation(), Sound.BLOCK_ANVIL_HIT, 0.5F, 0.7F);
+        }, 1);
     }
 
 }

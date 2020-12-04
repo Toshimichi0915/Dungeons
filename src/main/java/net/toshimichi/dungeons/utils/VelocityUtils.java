ac77ba2934dd3b0313ba5252fcf64f7d8d3db7c6
Toshimichi0915 <main@toshimichi.net>
2020-12-05 08:46:42 +0900

    Add Javadoc for VelocityUtils

--- src/main/java/net/toshimichi/dungeons/utils/VelocityUtils.java
+++ src/main/java/net/toshimichi/dungeons/utils/VelocityUtils.java
@@ -5,14 +5,27 @@ import org.bukkit.util.Vector;
 
 import java.util.WeakHashMap;
 
+/**
+ * プレイヤーの移動速度を管理します.
+ */
 public class VelocityUtils {
 
     private static final WeakHashMap<Player, Vector> vectors = new WeakHashMap<>();
 
+    /**
+     * プレイヤーの移動速度を返します.
+     * @param player プレイヤー
+     * @return プレイヤーの移動速度
+     */
     public static Vector getVelocity(Player player) {
         return vectors.getOrDefault(player, new Vector(0, 0, 0));
     }
 
+    /**
+     * プレイヤーの移動速度を設定します.
+     * @param player プレイヤー
+     * @param velocity プレイヤーの移動速度
+     */
     public static void setVelocity(Player player, Vector velocity) {
         vectors.put(player, velocity);
     }
